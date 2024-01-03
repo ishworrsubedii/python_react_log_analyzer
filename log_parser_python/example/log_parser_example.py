192e61b36f46b0fdf8fac4c5788c4ebdda0cc451
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-03 22:26:54 +0545

    added example for log parser using multiprocess

---
+++ log_parser_python/example/log_parser_example.py
@@ -0,0 +1,34 @@
+import multiprocessing
+from log_parser_python.services.log_pharser.log_parser_service import LogParserService
+
+class LogParserServiceExample:
+    def __init__(self, log_file_path):
+        self.log_parser = LogParserService(log_file_path)
+        self.log_lines = self.log_parser.read_log_file()
+        self.pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+
+    def __del__(self):
+        self.pool.close()
+        self.pool.join()
+
+    def ip_info(self):
+        ip_addresses = self.pool.map(self.log_parser.extract_ip_address, self.log_lines)
+        return ip_addresses
+
+    def os_info(self):
+        os_info = self.pool.map(self.log_parser.extract_os_info, self.log_lines)
+        return os_info
+
+    def browser_info(self):
+        browser_info = self.pool.map(self.log_parser.extract_browser_info, self.log_lines)
+        return browser_info
+
+    def time_info(self):
+        time_info = self.pool.map(self.log_parser.extract_times_from_log_file, self.log_lines)
+        return time_info
+
+if __name__ == '__main__':
+    log_file_path = '/home/ishwor/Documents/c++/distributed parallel/individual_react/individual_dpc/log_parser_python/resources/logfiles.log'
+    log_parser_example = LogParserServiceExample(log_file_path)
+    ip = log_parser_example.time_info()
+    print(ip)
---
+++ log_parser_python/services/log_pharser/log_parser_service.py
@@ -0,0 +1,108 @@
+import multiprocessing
+import re
+
+
+class LogParserService:
+    def __init__(self, log_file_path):
+        self.log_file_path = log_file_path
+
+    def extract_ip_address(self, log_line):
+        """
+        Extracts IP address from the log line
+
+        :return: IP address
+        """
+        parts = log_line.split('"')
+        ip_address = parts[0].split()[0]
+
+        return ip_address
+
+    def extract_os_info(self, log_line):
+        """
+        Extracts operating system information from the log line
+
+        :return: operating system information
+        """
+        parts = log_line.split('"')
+        user_agent = parts[-2]
+
+        os_info = user_agent.split('(')[1].split(')')[0]
+
+        return os_info
+
+    def extract_browser_info(self, log_line):
+        """
+        Extracts browser information from the log line
+
+        :return: browser information
+        """
+        parts = log_line.split('"')
+        user_agent = parts[-2]
+
+        browser_info = user_agent.split('(')[0].strip()
+
+        return browser_info
+
+    def extract_times_from_log_file(self, log_line):
+        """
+        Extracts timestamps from the log file
+
+        :return: timestamps
+        """
+        timestamps = []
+
+        for line in log_line:
+            timestamp = self.extract_time_from_line(line)
+            if timestamp:
+                timestamps.append(timestamp)
+
+        return timestamps
+
+    @staticmethod
+    def extract_time_from_line(line):
+        """
+        Extracts timestamp from the log line
+
+        :param line: log line
+        :return: timestamp
+        """
+        start_index = line.find('[')
+        end_index = line.find(']')
+
+        if start_index != -1 and end_index != -1:
+            timestamp_section = line[start_index + 1:end_index]
+            date_time_parts = timestamp_section.split()
+
+            if len(date_time_parts) == 2:
+                date = date_time_parts[0]
+                time = date[12:]
+                return time
+
+        return None
+
+    def read_log_file(self):
+        """
+        Reads the log file and returns the list of lines
+
+        :return: list of lines in the file
+        """
+        with open(self.log_file_path, 'r') as file:
+            log_line = file.readlines()
+        return log_line
+
+
+if __name__ == '__main__':
+    log_file_path = '/home/ishwor/Documents/c++/distributed parallel/individual_react/individual_dpc/log_parser_python/resources/archive/logfiles.log'
+
+    log_parser = LogParserService(log_file_path)
+    log_lines = log_parser.read_log_file()
+
+    num_processes = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    ip_addresses = pool.map(log_parser.extract_ip_address, log_lines)
+    os_info = pool.map(log_parser.extract_os_info, log_lines)
+    browser_info = pool.map(log_parser.extract_browser_info, log_lines)
+    time_info = pool.map(log_parser.extract_times_from_log_file, log_lines)
+
+    print(ip_addresses)
