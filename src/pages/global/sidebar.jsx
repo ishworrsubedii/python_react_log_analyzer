7d0fcca799137704f4c231c98c564ba4889dce77
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-07 23:05:15 +0545

    Add SidebarItems for Bar, Pie, Line, and Geography charts with respective icons

---
+++ src/pages/global/sidebar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { Sidebar as ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const SidebarItem = ({ title, to, icon, selected, setSelected }) => {
+  return (
+    <Link to={to}>
+      <MenuItem
+        active={selected === title}
+        style={{
+          backgroundColor: "#000",
+          color: "#fff",
+        }}
+        onClick={() => setSelected(title)}
+        icon={icon}
+      >
+        <Typography color="#fff">{title}</Typography>
+      </MenuItem>
+    </Link>
+  );
+};
+
+const CustomSidebar = () => {
+  const [selected, setSelected] = useState("");
+
+  return (
+    <ProSidebar
+      style={{
+        width: "200px",
+        height: "500px",
+        background: "#000",
+      }}
+    >
+      <Menu iconShape="square">
+        <Box sx={{ background: "#000", color: "#fff", padding: 2 }}>
+          <SidebarItem
+            title="Dashboard"
+            to="/"
+            icon={<HomeOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <Typography sx={{ m: "15px 0 5px 20px" }}>Charts</Typography>
+          <SidebarItem
+            title="Bar Chart"
+            to="/bar"
+            icon={<BarChartOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <SidebarItem
+            title="Pie Chart"
+            to="/pie"
+            icon={<PieChartOutlineOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <SidebarItem
+            title="Line Chart"
+            to="/line"
+            icon={<TimelineOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <SidebarItem
+            title="Geography Chart"
+            to="/geography"
+            icon={<MapOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        </Box>
+      </Menu>
+    </ProSidebar>
+  );
+};
+
+export default CustomSidebar;
