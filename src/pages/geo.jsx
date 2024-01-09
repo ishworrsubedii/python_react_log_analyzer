0ac756a8733c7a03b008b56263edff57fe1c6251
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-10 02:01:17 +0545

    Implement GeoChart with dynamic data loading

--- src/pages/geo.jsx
+++ src/pages/geo.jsx
@@ -1,56 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart } from 'react-google-charts';
 
 const GeoChartExample = () => {
-  const [dataObject, setDataObject] = useState({});
+  const [data, setData] = useState([['Country', 'Value']]);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('http://127.0.0.1:8000/ip-counts', {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-        },
+    fetch('http://127.0.0.1:8000/country-info')
+      .then(response => response.json())
+      .then(data => {
+        const formattedData = Object.entries(data.message).map(([country, value]) => [country, value]);
+        setData(prevData => [...prevData, ...formattedData]);
       });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setDataObject(data);
-    }
-
-    fetchData();
   }, []);
 
-  const dataArray = [['Country', 'Value']];
-  for (const [country, value] of Object.entries(dataObject)) {
-    dataArray.push([country, value]);
-  }
-
   return (
-    
-    
-    <div style={{ width: '900px' }}
-    
-    >
-                    <h2 style={{textAlign:'center'}}>Geograph Visualization of IP  </h2> 
-                  
-
-    
+    <div style={{ width: '900px' }}>
+      <h2 style={{textAlign:'center'}}>Geo Chart</h2>
+      <br />
       <Chart
-        width={'00px'}
-        height={'400px'}
+        width={'1000px'}
+        height={'500px'}
         chartType="GeoChart"
-        data={dataArray}
+        data={data}
         options={{
-          colorAxis: { colors: ['green', 'yellow'] },
+          colorAxis: { colors: ['white', 'black', '#e31b23'] },
           backgroundColor: 'transparent',
         }}
       />
     </div>
   );
+
 };
 
 export default GeoChartExample;
